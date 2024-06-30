cba046ddfc623cdfac971df612979ada04393d3e
webexguruhasan <webexguruhasan95@gmail.com>
2024-07-01 00:03:38 +0600

    [COMPLETED]: App css design and Responsive design for mobile version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 
 import { FaCopy, FaExchangeAlt, FaVolumeUp } from 'react-icons/fa';
 import './css/styles.css';
-const copy = FaCopy
-const volume = FaVolumeUp
-const exchange = FaExchangeAlt
+
 function App() {
   return (
     <>
@@ -13,29 +11,48 @@ function App() {
         {/* app-left */}
         <div className='app__container--left'>
           {/* left-header-part */}
-          <div className='left__header'>
-            <button className='btn__detected'>Detected Language</button>
-            <button className='btn__english'>English</button>
-            <button className='btn__bengali'>Bengali</button>
+          <div className='app__header'>
+            <button className='btn btn__detected'>Detected Language</button>
+            <button className='btn btn__language'>English</button>
+            <button className='btn btn__language'>Bengali</button>
             <select name='languages' className='left__language--switch'>
-              <option value="language">English</option>
-              <option value="language">Bengalis</option>
+              <option value="en">English</option>
+              <option value="bn">Bengalis</option>
             </select>
           </div>
           {/* left-content-part */}
           <div className='content__area'></div>
           {/* left-footer-part */}
-          <div className='left__footer'></div>
+          <div className='app__footer'>
+            <div className='copy__sound--icon'>
+              <button className='btn__icon'><FaCopy className='icon' /></button>
+              <button className='btn__icon'><FaVolumeUp className='icon' /></button>
+            </div>
+            <button className='btn btn__detected'>Translate</button>
+          </div>
         </div>
 
         {/* app-right */}
         <div className='app__container--right'>
           {/* right-header-part */}
-        <div className='right__header'></div>
+        <div className='app__header'>
+        <button className='btn btn__language'>English</button>
+            <button className='btn btn__language'>Bengali</button>
+            <select name='languages' className='left__language--switch'>
+              <option value="en">English</option>
+              <option value="bn">Bengalis</option>
+            </select>
+            <button className='btn__icon'><FaExchangeAlt className='icon' /></button>
+        </div>
           {/* right-content-part */}
         <div className='content__area'></div>
           {/* right-footer-part */}
-        <div className='right__footer'></div>
+        <div className='app__footer'>
+        <div className='copy__sound--icon'>
+              <button className='btn__icon'><FaCopy className='icon' /></button>
+              <button className='btn__icon'><FaVolumeUp className='icon' /></button>
+            </div>
+        </div>
         </div>
 
       </div>
